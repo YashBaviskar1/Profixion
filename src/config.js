05a65472968126b9832d290a54c669f9a01c4971
Yash Baviskar <yashbav24@gmail.com>
2025-10-24 02:31:01 +0530

    dns fix

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
   (typeof import.meta !== 'undefined' && import.meta.env && import.meta.env.VITE_API_BASE_URL)
-  || " https://f356c7306c06.ngrok-free.app/api";
+  || " https://profixion.in/api";
 
 export default API_BASE_URL;
